8a53509a1d89e0b66578ac68342cb6b9f9160d91
yuichirosaito <l.0821.enjoy@icloud.com>
2021-01-05 21:53:29 +0900

    Facebook認証

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_04_101806) do
+ActiveRecord::Schema.define(version: 2021_01_05_100903) do
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "content"
@@ -34,6 +34,15 @@ ActiveRecord::Schema.define(version: 2021_01_04_101806) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "sns_credentials", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "provider"
+    t.string "uid"
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_sns_credentials_on_user_id"
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -48,4 +57,5 @@ ActiveRecord::Schema.define(version: 2021_01_04_101806) do
   end
 
   add_foreign_key "scores", "users"
+  add_foreign_key "sns_credentials", "users"
 end
