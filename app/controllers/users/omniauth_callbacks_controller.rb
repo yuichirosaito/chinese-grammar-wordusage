4d9c39063b9f35f559e575769938b6a09776a77c
yuichirosaito <l.0821.enjoy@icloud.com>
2021-01-06 11:48:23 +0900

    twitter認証

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -31,6 +31,10 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     authorization
    end
 
+   def twitter
+    authorization
+   end
+
    private
   
    def authorization
